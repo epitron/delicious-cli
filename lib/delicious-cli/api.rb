a6fcfd6035ff41771ed8d5ad6c63ca88a44cf4b2
epitron <chris@ill-logic.com>
2014-11-09 23:47:37 -0500

    Maximum number of links to download increased to 100,000

--- lib/delicious-cli/api.rb
+++ lib/delicious-cli/api.rb
@@ -18,7 +18,21 @@ class Delicious
     end
   end
 
+  #
+  # posts_all options:
+  # (from: https://github.com/SciDevs/delicious-api/blob/master/api/posts.md#v1postsall)
+  #
+  # &tag_separator=comma (optional) - (Recommended) Returns tags separated by a comma, instead of a space character. A space separator is currently used by default to avoid breaking existing clients - these default may change in future API revisions.
+  # &tag={TAG} (optional) — Filter by this tag.
+  # &start={xx} (optional) — Start returning posts this many results into the set.
+  # &results={xx} (optional) — Return up to this many results. By default, up to 1000 bookmarks are returned, and a maximum of 100000 bookmarks is supported via this API.
+  # &fromdt={CCYY-MM-DDThh:mm:ssZ} (optional) — Filter for posts on this date or later.
+  # &todt={CCYY-MM-DDThh:mm:ssZ} (optional) — Filter for posts on this date or earlier.
+  # &meta=yes (optional) — Include change detection signatures on each item in a ‘meta’ attribute. Clients wishing to maintain a synchronized local store of bookmarks should retain the value of this attribute - its value will change when any significant field of the bookmark changes.
+  #
   def self.posts_all(options={})
+    options = {:results => 100000}.merge(options)
+
     result = get('/posts/all', :query=>options)
     begin
       [result["posts"]["post"]].flatten
